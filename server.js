91bef2b81551b5ce43ce46fa0b19fa8acfd54353
Vexcompany <vexcorporation43@gmail.com>
2026-03-15 12:30:41 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const soundcloudRoutes = require('./api/soundcloud');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public')); 
+
+app.use('/api', soundcloudRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
